0dfbd3d5f2a66b92f031e2de97429304c2a57366
superyngo <superyngo@gmail.com>
2023-07-07 11:21:58 +0800

    UPDATE

--- src/components/myNav.vue
+++ src/components/myNav.vue
@@ -190,7 +190,7 @@ path {
 }
 
 /* for small screen */
-@media only screen and (max-width: 700px) {
+@media only screen and (max-width: 600px) {
   .navbar {
     bottom: 0;
     height: var(--navWidth);
@@ -214,7 +214,7 @@ path {
 }
 
 /* for large screen */
-@media only screen and (min-width: 700px) {
+@media only screen and (min-width: 600px) {
   .navbar {
     top: 0;
     height: 100vh;
